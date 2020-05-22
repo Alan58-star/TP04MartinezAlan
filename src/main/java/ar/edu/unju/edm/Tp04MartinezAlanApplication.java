83eedaa62b605eaacb55a35da48f29db4c080207
alumno <alumno@CIB8AEED12CA1A>
2020-05-22 03:24:26 -0300

    Iniacilazion del proyecto

---
+++ src/main/java/ar/edu/unju/edm/Tp04MartinezAlanApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04MartinezAlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04MartinezAlanApplication.class, args);
+	}
+
+}
