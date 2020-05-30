f93bb3826497af659bd90ca9c2c51f6864a1af3b
alumno <alumno@CIB8AEED12CA1A>
2020-05-30 15:43:06 -0300

    completar

--- src/main/java/ar/edu/unju/edm/controller/AplicacionController.java
+++ src/main/java/ar/edu/unju/edm/controller/AplicacionController.java
@@ -10,4 +10,28 @@ public class AplicacionController {
 	public String InicarAplicacion() {
 		return "index";
 	}
+	@GetMapping("/fixture")
+	public String Fixture(){
+		return "Fixture";
+	}
+	@GetMapping("/Equipos")
+	public String Equipos() {
+		return "Equipos";
+	}
+	@GetMapping("/home2")
+	public String InicarAplicacion2() {
+		return "index2";
+	}
+	@GetMapping("/veterinaria")
+	public String Veterinaria() {
+		return "Veterinaria";
+	}
+	@GetMapping("/voluntariado")
+	public String Voluntariado() {
+		return "Voluntariado";
+	}
+	@GetMapping("/adopciones")
+	public String Adopciones() {
+		return "Adopciones";
+	}
 }
